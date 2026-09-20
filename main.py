3aaa2ad4be99bf07a07891139ea057518bad0bed
Gabito <gabrielfernandescolombo@gmail.com>
2026-09-20 11:21:10 -0300

    passo8

--- main.py
+++ main.py
@@ -1,34 +1,42 @@
 import cv2
 
+from visao.features import FeatureExtractor
+
 
 def main():
+
+    # Cria o extrator
+    extractor = FeatureExtractor()
+
+    # Abre a câmera
     camera = cv2.VideoCapture(0)
 
     if not camera.isOpened():
-        print("Erro: não foi possível abrir a câmera.")
+        print("Erro ao abrir a câmera.")
         return
 
     while True:
+
         success, frame = camera.read()
 
         if not success:
-            print("Erro ao capturar frame.")
+            print("Erro ao capturar imagem.")
             break
 
-        # Tamanho da imagem da câmera
+        # Dimensões da imagem
         height, width = frame.shape[:2]
 
-        # Definindo a área onde o objeto ficará
-        x1 = int(width * 0.10)
-        x2 = int(width * 0.90)
+        # Define o ROI
+        x1 = int(width * 0.25)
+        x2 = int(width * 0.75)
 
-        y1 = int(height * 0.10)
-        y2 = int(height * 0.90)
+        y1 = int(height * 0.20)
+        y2 = int(height * 0.80)
 
-        # Recorta somente essa região
+        # Recorta o objeto
         roi = frame[y1:y2, x1:x2]
 
-        # Desenha o retângulo na imagem original
+        # Desenha o ROI
         cv2.rectangle(
             frame,
             (x1, y1),
@@ -37,7 +45,6 @@ def main():
             2
         )
 
-        # Texto na tela
         cv2.putText(
             frame,
             "Coloque o objeto aqui",
@@ -48,15 +55,25 @@ def main():
             2
         )
 
-        # Mostra a câmera
         cv2.imshow("Camera", frame)
+        cv2.imshow("ROI", roi)
 
-        # Mostra somente o recorte
-        cv2.imshow("Recorte", roi)
-
-        # ESC para sair
+        # Aperte ESPACO para testar o MobileNet
         key = cv2.waitKey(1) & 0xFF
 
+        if key == 32:
+
+            print("\nProcessando imagem...")
+
+            vector = extractor.extract(roi)
+
+            print("Vetor gerado!")
+            print("Dimensao:", vector.shape)
+
+            print("Primeiros valores:")
+            print(vector[:10])
+
+        # ESC para sair
         if key == 27:
             break
 
